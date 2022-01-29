132031711ee17e70ff447d757b9c868e0af8b4af
Patrick Oliveira <patrickoliveira9@hotmail.com.br>
2022-01-29 09:03:57 -0300

    Aula 2

---
+++ pages/chat.js
@@ -0,0 +1,3 @@
+export default function PaginadoChat() {
+  return <h1>Página do chat</h1>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
 import {Box, Button, Text, TextField, Image} from "@skynexui/components";
+import {useState} from "react";
+import {useRouter} from "next/router";
 import appConfig from "../config.json";
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
 
 function Title(props) {
   const Tag = props.tag || "h1";
@@ -67,19 +38,19 @@ function Title(props) {
 // export default HomePage;
 
 export default function PaginaInicial() {
-  const username = "POliveira29";
+  // const username = "POliveira29";
+  const [username, setUsername] = useState("");
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage:
-            "url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)",
+          backgroundColor: appConfig.theme.colors.neutrals[200],
+          backgroundImage: "url(https://imgur.com/CQsiflq.jpg)",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
           backgroundBlendMode: "multiply",
@@ -106,6 +77,11 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (event) {
+              event.preventDefault();
+
+              roteamento.push("/chat");
+            }}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -128,6 +104,11 @@ export default function PaginaInicial() {
             </Text>
 
             <TextField
+              value={username}
+              onChange={function handler(event) {
+                const valor = event.target.value;
+                setUsername(valor);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
